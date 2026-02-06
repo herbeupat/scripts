8b1f900fc59896b17981176c7029511bd0b825f6
Patrick Herbeuval <herbeupat@gmail.com>
2026-02-06 21:57:16 +0100

    Fix script not working because of &

--- update_discord.sh
+++ update_discord.sh
@@ -1,3 +1,4 @@
-wget -O discord.deb https://discord.com/api/download/stable?platform=linux&format=deb
+#!/bin/sh
+wget -O discord.deb "https://discord.com/api/download/stable?platform=linux&format=deb"
 sudo dpkg -i discord.deb
 rm discord.deb
